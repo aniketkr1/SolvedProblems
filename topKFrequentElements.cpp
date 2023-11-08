6cd61fde96e8cec33d2d4942c92efe75c2533a95
Aniket Kumar Pandit <aniketkumar10.16@gmail.com>
2023-11-08 12:28:38 +0530

    comment added in top k frequent elements

--- topKFrequentElements.cpp
+++ topKFrequentElements.cpp
@@ -1,3 +1,15 @@
+/*Given a non-empty array nums[] of integers of length N, find the top k elements which have the highest frequency in the array. If two numbers have same frequencies, then the larger number should be given more preference.
+
+Input: N = 6, nums = {1,1,1,2,2,3}, k = 2
+Output: {1, 2}
+
+Input:N = 8, nums = {1,1,2,2,3,3,3,4}, k = 2
+Output: {3, 2}
+Explanation: Elements 1 and 2 have the
+same frequency ie. 2. Therefore, in this
+case, the answer includes the element 2
+as 2 > 1.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
