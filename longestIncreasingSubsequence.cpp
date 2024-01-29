f6bbc62cecb721433f4d08e919347f73680c8ce2
Aniket Kumar Pandit <aniketkumar10.16@gmail.com>
2024-01-29 12:56:03 +0530

    2 programs added

---
+++ longestIncreasingSubsequence.cpp
@@ -0,0 +1,43 @@
+/*
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lengthOfLIS(std::vector<int>& nums) {
+    if (nums.empty()) {
+        return 0;
+    }
+
+    int n = nums.size();
+    std::vector<int> dp(n, 1);
+
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (nums[i] > nums[j]) {
+                dp[i] = std::max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return *std::max_element(dp.begin(), dp.end());
+}
+
+int main() {
+    vector<int> nums = {10,9,2,5,3,7,101,18};
+    std::cout << "Lenght of longest increasing subsequence: " << lengthOfLIS(nums);
+}
