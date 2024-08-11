65c074b4d1b935db3e63a20dc0e8058be63e4ac7
Aniket Kumar Pandit <aniketkumar10.16@gmail.com>
2024-08-11 13:54:50 +0530

    comments added

--- wordSearch.cpp
+++ wordSearch.cpp
@@ -14,14 +14,14 @@ Output: true
 Example 2:
 Input: board = [["A","B","C","E"],
                 ["S","F","C","S"],
-                ["A","D","E","E"]], 
+                ["A","D","E","E"]],
                 word = "SEE"
 Output: true
 
 Example 3:
 Input: board = [["A","B","C","E"],
                 ["S","F","C","S"],
-                ["A","D","E","E"]], 
+                ["A","D","E","E"]],
                 word = "ABCB"
 Output: false
 */
@@ -29,22 +29,31 @@ Output: false
 using namespace std;
 
 bool solve(int row, int col, int index, vector<vector<char>>& board, string word, int dx[], int dy[], int m, int n) {
+    // we reach the end of string word that means we found the word
     if (index >= word.size()) {
         return true;
     }
-    char temp = board[row][col];
-    board[row][col] = '#';
+
     for (int i = 0; i < 4; i++) {
         int nx = row + dx[i];
         int ny = col + dy[i];
 
+        // checking all 4 directions and if it is valid then then marking it with "#"
+        // so that we don't visit it again
         if (nx >= 0 && nx < m && ny >= 0 && ny < n && board[nx][ny] == word[index]) {
+            // making that cell as visited
+            char temp = board[row][col];
+            board[row][col] = '#';
+            // recursive call on that cell where the character is matched
+            // index will be index+1 (move the pointer to next character of the word)
             if (solve(nx, ny, index + 1, board, word, dx, dy, m, n) == true) {
                 return true;
             }
+            // backtrack(restore to origianl character so that we can check other possibilities)
+            board[row][col] = temp;
         }
     }
-    board[row][col] = temp;
+
     return false;
 }
 bool exist(vector<vector<char>>& board, string word) {
@@ -53,6 +62,7 @@ bool exist(vector<vector<char>>& board, string word) {
     char start = word[0];
     int dx[4] = { 1, 0, 0, -1 };
     int dy[4] = { 0, -1, 1, 0 };
+    // If any cell == first character of the word we will check
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             if (board[i][j] == start) {
@@ -66,11 +76,12 @@ bool exist(vector<vector<char>>& board, string word) {
 }
 
 int main() {
-    vector<vector<char>> board =  {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    vector<vector<char>> board = { {'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'} };
     string word = "ABCCED";
-    if(exist(board, word)) {
-        std::cout << word <<  " exist";
-    } else {
+    if (exist(board, word)) {
+        std::cout << word << " exist";
+    }
+    else {
         std::cout << word << " don't exist";
     }
 }
